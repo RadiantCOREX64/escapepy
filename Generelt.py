1e697e09fce87fb9a5c3caf9050a507db1ccf438
Sina Parsa <Sina.P64@Outlook.com>
2025-05-12 21:16:16 +0330

    Add files via upload

---
+++ Generelt.py
@@ -0,0 +1,25 @@
+##escape sequence
+###cotation før ord bruges \"" 
+
+message = "Hello \"World"
+
+print(message)
+
+## I to forskellige linier bruges \n
+
+x = "Hello \nWorld"
+
+print(x)
+
+## \t svarer til tab
+
+print(" left\tRight ")
+
+names = " Thomas\tJensen"
+
+print(names)
+
+print(type(names))
+
+##example for det hele
+print("welcome\b to \t course\n python")
